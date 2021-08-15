4b1a0205bcb9eca59aea167805a2ed7292782158
Gokhan <gokhanbilgin5@gmail.com>
2021-08-15 19:34:35 +0300

    Add product list

--- product-service/src/main/java/com/example/productservice/ProductController.java
+++ product-service/src/main/java/com/example/productservice/ProductController.java
@@ -4,11 +4,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ProductController {
 
+    List<ProductInfo> productList = new ArrayList<ProductInfo>();
+
     @GetMapping("product/details/{productid}")
-    public Product getProductInfo(@PathVariable Long productid) {
-        return new Product(productid, "iPhone", "iPhone 12", 25000, true);
+    public Product getProductDetails(@PathVariable Long productid) {
+        //Get Name and Desc from product-service
+        ProductInfo productInfo = getProductInfo(productid);
+        //Get Price from pricing-service
+        //Get Stock Avail from inventory-service
+        return new Product(productInfo.getProductID(), productInfo.getProductName(), productInfo.getProductDesc(), 25000, true);
+    }
+
+    private ProductInfo getProductInfo(Long productid) {
+        populateProductList();
+        for (ProductInfo p : productList) {
+            if (productid.equals(p.getProductID())) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    private void populateProductList() {
+        productList.add(new ProductInfo(101L, "iPhone", "iPhone 12"));
+        productList.add(new ProductInfo(102L, "Book", "Clean Code Book"));
+        productList.add(new ProductInfo(103L, "MacBook", "MacBook Air"));
     }
 }
---
+++ product-service/src/main/java/com/example/productservice/ProductInfo.java
@@ -0,0 +1,14 @@
+package com.example.productservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductInfo {
+    private Long productID;
+    private String productName;
+    private String productDesc;
+}
